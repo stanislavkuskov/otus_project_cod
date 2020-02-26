3de20e7fe824629c3d432809b6db49d52d52cd81
stanislavkuskov <kuskov.stanislav@gmail.com>
2020-02-26 16:17:40 +0300

    test travis without opencv

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
-//https://pydeeplearning.com/opencv/install-opencv-with-c-on-ubuntu-18-04/
-//https://gist.github.com/Randommood/478ccd806511d45febc1
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/videoio/videoio.hpp"
+////https://pydeeplearning.com/opencv/install-opencv-with-c-on-ubuntu-18-04/
+////https://gist.github.com/Randommood/478ccd806511d45febc1
+//#include "opencv2/core/core.hpp"
+//#include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/videoio/videoio.hpp"
 #include <iostream>
 
-using namespace cv;
+//using namespace cv;
 using namespace std;
 
-void drawText(Mat & image);
+//void drawText(Mat & image);
 
 int main()
 {
-    cout << "Built with OpenCV " << CV_VERSION << endl;
-    Mat image;
-    VideoCapture capture;
-    capture.open(0);
-    capture.set(3,640);
-    capture.set(4,480);
-
-    while (capture.isOpened())
-    {
-        capture >> image;
-        imshow("Sample", image);
-        if(waitKey(10) >= 0)
-            break;
-    }
+    cout << "helloworld" << endl;
+//    cout << "Built with OpenCV " << CV_VERSION << endl;
+//    Mat image;
+//    VideoCapture capture;
+//    capture.open(0);
+//    capture.set(3,640);
+//    capture.set(4,480);
+//
+//    while (capture.isOpened())
+//    {
+//        capture >> image;
+//        imshow("Sample", image);
+//        if(waitKey(10) >= 0)
+//            break;
+//    }
     return 0;
 }
