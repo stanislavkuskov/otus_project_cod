82b36b1c721e00d283cab5de51aaaa4fd548960a
stanislavkuskov <kuskov.stanislav@gmail.com>
2020-03-11 21:45:34 +0300

    create example of string publish

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,17 +1,26 @@
 //
 // Created by kuskov on 11.03.2020.
 //
-
 #include "publisher.h"
 
+Publisher::Publisher(): context_(1), pub_(context_, ZMQ_PUB) {
+    pub_.bind("tcp://*:5563");
+    printf("Starting server...\n");
+}
+
 void Publisher::setDetect(std::vector<DetectedObject> detected_objs) {
+    // TODO make it atomic
     detected_objs_ = std::move(detected_objs);
 }
 
 void Publisher::transmitDetect() {
+    std::string data_text = "Text";
     while (true){
-        std::cout << "transmit" << std::endl;
-//        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // TODO make it atomic
+        zmq_send(pub_, topic_.data(), topic_.size(), ZMQ_SNDMORE);
+        zmq_send(pub_, data_text.data(), data_text.size(), 0);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
 
 }
--- src/publisher.h
+++ src/publisher.h
@@ -6,14 +6,21 @@
 #define COD_PUBLISHER_H
 
 #include "multiheaders.h"
+#include <zmq.hpp>
 
 class Publisher {
 public:
+    Publisher();
     void setDetect(std::vector<DetectedObject> detected_objs);
     void transmitDetect();
 
 private:
+    std::string topic_ = "cod";
     std::vector<DetectedObject> detected_objs_;
+
+    zmq::context_t context_;
+    zmq::socket_t pub_;
+
 };
 
 
